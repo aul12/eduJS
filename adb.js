a5af76163f9f73190a0632f1f4628eee2931e32d
Paul Nykiel <paul.nykiel@gmail.com>
2016-01-23 16:22:46 +0100

    Rewrote File Dialogs

---
+++ adb.js
@@ -0,0 +1,70 @@
+function launchADB(){
+    //adb shell am start -a android.intent.action.VIEW -n org.mozilla.firefox/.App -d 'file:///sdcard/eduJS/exampleInput/index.html'
+
+    var client = adb.createClient();
+
+    client.listDevices()
+        .then(function(devices) {
+            return Promise.map(devices, function(device) {
+                return client.shell(device.id, "am start -a android.intent.action.VIEW -n org.mozilla.firefox/.App -d 'file:///sdcard/eduJS/"+fname+"/index.html'")
+                    // Use the readAll() utility to read all the content without
+                    // having to deal with the events. `output` will be a Buffer
+                    // containing all the output.
+                    .then(adb.util.readAll)
+                    .then(function(output) {
+                        console.log('[%s] %s', device.id, output.toString().trim())
+                    })
+            })
+        })
+        .then(function() {
+            showSnackbar('Erfolgreich gestartet')
+        })
+        .catch(function(err) {
+            showSnackbar('Fehler:', err.stack)
+        });
+}
+
+function installADB(){
+    if(fname == null)
+        $("#modalSave").modal('show');
+    else{
+        var client = adb.createClient();
+
+        var timeout = setTimeout(function(){
+            $.snackbar({content: 'Fehler beim installieren!'});
+        }, 1000);
+
+        client.listDevices()
+            .then(function(devices) {
+                return Promise.map(devices, function(device) {
+                    return client.push(device.id, "./save/" + fname + "/index.html", '/sdcard/eduJS/'+fname+'/index.html')
+                        .then(function(transfer) {
+                            return new Promise(function(resolve, reject) {
+                                transfer.on('progress', function(stats) {
+                                    console.log('[%s] Pushed %d bytes so far',
+                                        device.id,
+                                        stats.bytesTransferred)
+                                });
+                                transfer.on('end', function() {
+                                    console.log('[%s] Push complete', device.id);
+                                    resolve();
+                                    clearTimeout(timeout);
+                                    showSnackbar('Erfolgreich installiert!');
+                                    launchADB();
+
+                                });
+                                transfer.on('error', reject);
+                            })
+                        })
+                })
+            })
+            .then(function() {
+                //$.snackbar({content: 'Erfolgreich installiert!'});
+            })
+            .catch(function(err) {
+                $.snackbar({content: 'Fehler beim Installieren: '+err});
+                console.log(err);
+            });
+    }
+}
+
---
+++ save.js
@@ -0,0 +1,72 @@
+var fname = null;
+
+$("#menuSave").click(function(){
+    if(fname == null)
+        $("#modalSave").modal('show');
+    else{
+        fs.writeFile("./save/" + fname + "/ui.save", editor.document.getBody().getHtml(), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+        fs.writeFile("./save/" + fname + "/js.save", codeMirror.getValue(), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+        fs.writeFile("./save/" + fname + "/index.html", composeHTML(), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+    }
+});
+
+$("#btnOpen").click(function(){
+    fname = $("#inputFNameOpen").val();
+
+    fs.readFile("./save/" + fname + "/ui.save", function (err, data) {
+        if (err)
+            showSnackbar(err);
+        editor.document.getBody().setHtml(String(data));
+    });
+    fs.readFile("./save/" + fname + "/js.save", function (err, data) {
+        if (err)
+            showSnackbar(err);
+        codeMirror.setValue(String(data));
+    });
+    $('a[href="#design"]').tab('show');
+});
+
+$("#btnSave").click(function(){
+    fname = $("#inputFNameSave").val();
+
+    try {
+        fs.mkdirSync("./save/" + fname);
+    }
+    catch (e) {
+    }
+
+    fs.writeFile("./save/" + fname + "/ui.save", editor.document.getBody().getHtml(), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+    fs.writeFile("./save/" + fname + "/js.save", codeMirror.getValue(), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+    fs.writeFile("./save/" + fname + "/index.html", composeHTML(), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+
+});

7e91eb0e779df077961c90c43ae26fb4fc5beab3
Paul Nykiel <paul.nykiel@gmail.com>
2016-01-23 10:40:35 +0100

    Launch App with ADB

--- editor.js
+++ editor.js
@@ -268,4 +268,47 @@ function openDevice(){
             $.snackbar({content: 'Fehler beim Installieren: '+err});
             console.log(err);
         })
+
+    //adb shell am start -a android.intent.action.VIEW -n org.mozilla.firefox/.App -d 'file:///sdcard/eduJS/exampleInput/index.html'
+
+
+    client.listDevices()
+        .then(function(devices) {
+            return Promise.map(devices, function(device) {
+                return client.shell(device.id, "am start -a android.intent.action.VIEW -n org.mozilla.firefox/.App -d 'file:///sdcard/eduJS/exampleInput/index.html'")
+                    // Use the readAll() utility to read all the content without
+                    // having to deal with the events. `output` will be a Buffer
+                    // containing all the output.
+                    .then(adb.util.readAll)
+                    .then(function(output) {
+                        console.log('[%s] %s', device.id, output.toString().trim())
+                    })
+            })
+        })
+        .then(function() {
+            console.log('Done.')
+        })
+        .catch(function(err) {
+            console.error('Something went wrong:', err.stack)
+        });
+
+        client.listDevices()
+        .then(function(devices) {
+            return Promise.map(devices, function(device) {
+                return client.shell(device.id, 'echo $RANDOM')
+                    // Use the readAll() utility to read all the content without
+                    // having to deal with the events. `output` will be a Buffer
+                    // containing all the output.
+                    .then(adb.util.readAll)
+                    .then(function(output) {
+                        showSnackbar('[%s] %s', device.id, output.toString().trim())
+                    })
+            })
+        })
+        .then(function() {
+            showSnackbar('Done.')
+        })
+        .catch(function(err) {
+            showSnackbar('Something went wrong:', err.stack)
+        })
 }

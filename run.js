32016790588f4c3b16e4f71347e48d70c31c1843
Paul Nykiel <paul.nykiel@gmail.com>
2016-01-23 17:45:58 +0100

    Bugfixes

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 $("#btnRun").click(function(){
     switch ($('input[name=radioDevice]:checked', '#formDevice').val()){
         case 'preview':
+            update();
             $("#modalPreview").modal();
             break;
         case 'adb':
